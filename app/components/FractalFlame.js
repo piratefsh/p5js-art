29e64421bb9c041344ea94349853e818bab574fe
Sher Minn Chong <sherminn.chong@viki.com>
2016-06-23 16:30:04 +0800

    Implement PDJ variation

--- app/components/FractalFlame.js
+++ app/components/FractalFlame.js
@@ -11,7 +11,7 @@ export default class FractalFlame{
     this.step = (this.x2 - this.x1) / (2.321 * p.width);;
   }
 
-   draw() {
+  draw() {
       const p = this.p;
 
       if (this.stop) return;
@@ -33,7 +33,7 @@ export default class FractalFlame{
       const p = this.p;
       const offsetx = 0.003 * p.randomGaussian();
       const offsety = 0.003 * p.randomGaussian();
-      const v = this.sinusoidal(p.createVector(x, y), 3);
+      const v = this.pdj(p.createVector(x, y), 1, [0.1, 1.9, -0.8, -1.2]);
 
       let xx = p.map(v.x + offsetx, this.x1, this.x2, this.NUM_LINES, p.width - this.NUM_LINES);
       let yy = p.map(v.y + offsety, this.y1, this.y2, this.NUM_LINES, p.height - this.NUM_LINES);
@@ -41,7 +41,21 @@ export default class FractalFlame{
       p.point(xx, yy);
     };
 
-    sinusoidal(point, r) {
-        return this.p.createVector(r * Math.sin(point.x), r * Math.sin(point.y))
+    sinusoidal(point, scale) {
+        return this.p.createVector(scale * Math.sin(point.x), scale * Math.sin(point.y))
+    };
+
+    hyperbolic(point, scale){
+      const r = point.mag() + 1.0e-10;
+      const theta = Math.atan2(point.x, point.y);
+      const x = scale * Math.sin(theta) / r;
+      const y = scale * Math.cos(theta) * r;
+      return this.p.createVector(x, y);
+    };
+
+    pdj(point, scale, params){
+      const x = scale * (Math.sin(params[0] * point.y) - Math.cos(params[1] * point.x))
+      const y = scale * (Math.sin(params[2] * point.x) - Math.cos(params[3] * point.y))
+      return this.p.createVector(x, y);
     }
 }
