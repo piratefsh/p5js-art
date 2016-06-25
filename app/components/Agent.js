80cda9ad2722c79fa8625bf0315d6e4fa6f1896f
Sher Minn Chong <sherminn.chong@gmail.com>
2016-06-25 18:03:41 +0800

    More stuff

--- app/components/Agent.js
+++ app/components/Agent.js
@@ -1,7 +1,7 @@
 export default class Agent{
-    constructor(p, x, y, noiseScale=30, noiseStrength=10) {
-        this.pos = new p.createVector(x, y);
-        this.prevPos = new p.createVector(x, y);
+    constructor(p, x, y, z, noiseScale=30, noiseStrength=10) {
+        this.pos = new p.createVector(x, y, z);
+        this.prevPos = new p.createVector(x, y, z);
 
         this.stepSize = p.random(1, 5);
         this.angle = 0;
@@ -10,27 +10,49 @@ export default class Agent{
         this.noiseStrength = noiseStrength;
 
         this.p = p;
+
+        this.segments = [];
     }
 
     draw(p=this.p){
+        p.push();
+        p.translate(-p.width/2, p.height/2, 0)
         this.angle = p.noise(this.pos.x / this.noiseScale, this.pos.y / this.noiseScale) * this.noiseStrength 
+        this.angleZ = p.noise(this.pos.z/this.noiseScale) * this.noiseStrength;
         // this.angle = p.map(this.angle, 0, 1, 0, p.PI) ;;
-        p.strokeWeight(0.7);
-        p.stroke(p.map(this.noiseStrength, 10, 20, 90, 50), 50)
+        // p.stroke(p.map(this.noiseStrength, 10, 20, 250, 100), 50)
 
-        this.pos.x += Math.cos(this.angle) * this.stepSize;
+        this.pos.x += Math.cos(this.angleZ) * Math.cos(this.angle) * this.stepSize;
         this.pos.y += Math.sin(this.angle) * this.stepSize;
+        this.pos.z += Math.sin(this.angleZ) *  Math.cos(this.angle) * 20;
 
+        // p.background(0)
+        // p.fill(255)
+        // if(this.pos.x < 0 || this.pos.x > p.width || this.pos.y < 0 || this.pos.y > p.height){
+        //     this.pos.x = p.random(10, p.width-10);
+        //     this.pos.y = p.random(10, p.height-10);
+        //     this.prevPos.set(this.pos);
+        // }
 
-        if(this.pos.x < 0 || this.pos.x > p.width || this.pos.y < 0 || this.pos.y > p.height){
-            this.pos.x = p.random(10, p.width-10);
-            this.pos.y = p.random(10, p.height-10);
-            this.prevPos.set(this.pos);
-        }
-        p.line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y);
+        // p.line(this.prevPos.x, this.prevPos.y, this.prevPos.z, this.pos.x, this.pos.y, this.pos.z);
+
+        p.ambientMaterial(255, 0, 0);
+
         this.prevPos.set(this.pos);
+        this.noiseStrength += 0.05;
+        p.strokeWeight(p.map(this.noiseStrength, 10, 50, 1, 10));
 
+        this.segments.push(this.pos.copy());
 
-        this.noiseStrength += 0.1;
+        p.beginShape();
+        this.segments.forEach((s) => {
+            p.vertex(s.x, s.y, s.z);
+        })
+        p.endShape();
+
+        // p.translate(this.pos.x, this.pos.y, this.pos.z);
+        // p.sphere(10, 10);
+
+        p.pop();
     }
 }
--- app/index.js
+++ app/index.js
@@ -7,30 +7,58 @@ const sketch = p => {
     const step = 30;
     const numAgents = p.width * p.height / (step * step)
     const agents = new Array(numAgents);
+    let zoom = 50;
     p.setup = () => {
-        p.createCanvas(500, 500);
+        p.createCanvas(600, 600, p.WEBGL);
         p.reset();
         p.frameRate(24);
     };
 
     p.reset = () => {
+
         p.background(240);
+        p.ambientLight(255, 0, 0);
+        p.normalMaterial()
         for(let i = 0; i < p.width; i+=step){
             for(let j = 0; j < p.height; j+=step){
 
                 const x = p.random(0, p.width)
                 const y = p.random(0, p.height)
-                const noiseScale = 40;
+                const z = p.random(-10, 10)
+                const noiseScale = 50;
                 const noiseStrength = 10; p.random(5, 10); 
-                agents[i * p.width/step + j] = new Agent(p, x, y, noiseScale, noiseStrength);
+                agents[i * p.width/step + j] = new Agent(p, x, y, z, noiseScale, noiseStrength);
             }
         }
     };
 
     p.draw = () =>{
+        p.background(0)
+        p.translate(0, 0, zoom);
+        p.strokeWeight(0.7);
+        // p.fill(255, 200, 10);
+        p.rotateX(p.map(p.mouseY, 0, p.width, 0, p.PI))
+        p.rotateY(p.map(p.mouseX, 0, p.width, 0, p.PI))
+        // p.camera(p.mouseX, p.height/2, (p.height/2) / Math.tan(p.PI/6), p.mouseX, p.height/2, 0, 0, 1, 0);
         agents.forEach((a) => {
             a.draw();
         })
+
+    }
+
+    p.keyPressed = () => {
+        if(p.key == 'Q'){
+            zoom += 50;
+            console.log(zoom)
+        }
+        if(p.key == 'W'){
+            zoom -= 50;
+            console.log(zoom)
+        }
+
+        if(p.key == 'S'){
+            p.save('omg.png')
+        }
     }
 };
 
