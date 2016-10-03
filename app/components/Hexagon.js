454faf936439d3dc5bcef85a20afce273b8739bd
Sher Minn Chong <sherminn.chong@gmail.com>
2016-10-03 11:01:55 +0800

    Store

--- app/components/Hexagon.js
+++ app/components/Hexagon.js
@@ -1,36 +1,83 @@
 export default class Hexagon {
-  constructor(p5, center) {
+  constructor(p5, center, rotation) {
     this.p5 = p5;
     this.edgeLen = 50;
     this.SIDES = 6;
     this.center = center;
-
+    this.rotation = rotation;
     this.edges = [];
     this.getEdges();
   }
 
   getEdges() {
     const p5 = this.p5;
-    const point = p5.createVector(0, this.edgeLen);
+    const point = p5.createVector(5, this.edgeLen);
     for (let i = 0; i < this.SIDES + 1; i++) {
       this.edges.push(point.copy());
       point.rotate(p5.PI / 3);
     }
   }
 
+  getEscherEdges() {
+    const p5 = this.p5;
+
+    const point = p5.createVector(0, this.edgeLen);
+    point.rotate(p5.TWO_PI / 3);
+    const APrime = point.copy();
+    point.rotate(p5.TWO_PI / 3);
+    const BPrime = point.copy();
+    point.rotate(p5.TWO_PI / 3);
+    const CPrime = point.copy();
+    // p5.push();
+    // p5.translate(this.center.x, this.center.y);
+    // p5.beginShape();
+    // p5.vertex(APrime.x, APrime.y);
+    // p5.vertex(BPrime.x, BPrime.y);
+    // p5.vertex(CPrime.x, CPrime.y);
+    // p5.vertex(APrime.x, APrime.y);
+    // p5.endShape();
+    // p5.pop();
+
+    const randomPoint = p5.createVector(-40, 28);
+    const B = APrime.copy().add(randomPoint);
+    randomPoint.rotate(-p5.TWO_PI / 3);
+    const C = APrime.copy().add(randomPoint);
+
+    const AB = C.copy().sub(BPrime);
+    AB.rotate(-p5.TWO_PI / 3);
+    const A = BPrime.copy().add(AB);
+
+    this.edges = [APrime, C, BPrime, A, CPrime, B, APrime];
+  }
+
+  width() {
+    const x = this.edges.map(e => e.x);
+    const w = Math.max.apply(null, x) - Math.min.apply(null, x);
+    return w;
+  }
+
+  reflect(normal) {
+    const reflected = this.edges.map((e) => {
+      const dotP = e.dot(normal)
+      const multP = normal.mult(dotP * 2);
+      const res = e.copy().sub(multP)
+      return res;
+    });
+  }
+
   draw() {
     const p5 = this.p5;
-    p5.push()
-    p5.stroke(0, 0);
-    p5.fill(150, p5.random(150, 200), p5.random(150, 200));
-
+    p5.push();
+    p5.stroke(0, 255);
+    p5.fill(150, p5.random(150, 200), p5.random(150, 200), 100);
     p5.translate(this.center.x, this.center.y);
+    p5.rotate(this.rotation);
 
     p5.beginShape();
     this.edges.forEach((e) => {
       p5.vertex(e.x, e.y);
     });
     p5.endShape();
-    p5.pop()
+    p5.pop();
   }
 }
--- app/components/HexagonTesselation.js
+++ app/components/HexagonTesselation.js
@@ -3,7 +3,7 @@ import Hexagon from './Hexagon';
 export default class HexagonTesselation {
   constructor(p5) {
     this.p5 = p5;
-    this.tiles = {};
+    this.tiles = [];
     this.makeTiles();
     console.log(this.tiles);
   }
@@ -13,35 +13,34 @@ export default class HexagonTesselation {
     const hexs = [];
 
     let hex = new Hexagon(p5, p5.createVector(0, 0));
-    this.tiles[[0, 0]] = hex;
+    this.tiles.push(hex)
     hexs.push(hex);
 
-
-    const len = (hex.edgeLen) * Math.sqrt(3);
-
-    while (hexs.length > 0) {
+    let go = true;
+    while (go && hexs.length > 0) {
       hex = hexs.shift();
-      const center = hex.center.copy();
-      center.x += len;
-      for (let i = 0; i < 6; i++) {
-        if (center.x < p5.width + len && center.x > 0 &&
-          center.y < p5.height + len && center.y > -len) {
-          const neighbour = new Hexagon(p5, center.copy());
-          if (!this.tiles[[center.x, center.y]]) {
-            this.tiles[[center.x, center.y]] = neighbour;
-            hexs.push(neighbour);
-          }
+      for (let i = 1; i < hex.edges.length; i++) {
+        const curr = hex.edges[i];
+        const prev = hex.edges[i - 1];
+        const normal = curr.sub(prev).normalize();
+
+        const neighbour = new Hexagon(p5, hex.center);
+        neighbour.reflect(normal);
+        const center = neighbour.edges[0];
+        if (center.x > 0 && center.x < p5.width &&
+          center.y > 0 && center.y < p5.height) {
+          hexs.push(neighbour);
+          this.tiles.push(neighbour);
         }
-        center.rotate(p5.TWO_PI/6);
       }
+      go = false;
     }
   }
 
   draw() {
     const p5 = this.p5;
-    // p5.translate(p5.width / 2, p5.height / 2);
-    for (const pos in this.tiles) {
-      this.tiles[pos].draw();
-    }
+    p5.translate(p5.width / 2, p5.height / 2);
+    // this.tiles.forEach(t => t.draw())
+    this.tiles[0].draw()
   }
 }
