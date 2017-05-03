ef62c1af50039674c5f47ccf23db1a950ef273b4
Sher Minn Chong <sherminn.chong@viki.com>
2017-05-03 17:00:54 +0800

    Add hexagon shape

--- app/components/Hexagon.js
+++ app/components/Hexagon.js
@@ -1,83 +1,28 @@
-export default class Hexagon {
-  constructor(p5, center, rotation) {
-    this.p5 = p5;
-    this.edgeLen = 50;
-    this.SIDES = 6;
-    this.center = center;
+import Shape from './Shape';
+import { p } from '../P5Instance';
+
+class Hexagon extends Shape {
+  constructor(length=20, x=0, y=0, rotation=0, translation=p.createVector(0, 0)) {
+    const point = p.createVector(x, y);
+    super(Hexagon.generatePoints(length));
+    this.length = length;
     this.rotation = rotation;
-    this.edges = [];
-    this.getEdges();
-  }
-
-  getEdges() {
-    const p5 = this.p5;
-    const point = p5.createVector(5, this.edgeLen);
-    for (let i = 0; i < this.SIDES + 1; i++) {
-      this.edges.push(point.copy());
-      point.rotate(p5.PI / 3);
-    }
-  }
-
-  getEscherEdges() {
-    const p5 = this.p5;
-
-    const point = p5.createVector(0, this.edgeLen);
-    point.rotate(p5.TWO_PI / 3);
-    const APrime = point.copy();
-    point.rotate(p5.TWO_PI / 3);
-    const BPrime = point.copy();
-    point.rotate(p5.TWO_PI / 3);
-    const CPrime = point.copy();
-    // p5.push();
-    // p5.translate(this.center.x, this.center.y);
-    // p5.beginShape();
-    // p5.vertex(APrime.x, APrime.y);
-    // p5.vertex(BPrime.x, BPrime.y);
-    // p5.vertex(CPrime.x, CPrime.y);
-    // p5.vertex(APrime.x, APrime.y);
-    // p5.endShape();
-    // p5.pop();
-
-    const randomPoint = p5.createVector(-40, 28);
-    const B = APrime.copy().add(randomPoint);
-    randomPoint.rotate(-p5.TWO_PI / 3);
-    const C = APrime.copy().add(randomPoint);
-
-    const AB = C.copy().sub(BPrime);
-    AB.rotate(-p5.TWO_PI / 3);
-    const A = BPrime.copy().add(AB);
-
-    this.edges = [APrime, C, BPrime, A, CPrime, B, APrime];
-  }
-
-  width() {
-    const x = this.edges.map(e => e.x);
-    const w = Math.max.apply(null, x) - Math.min.apply(null, x);
-    return w;
-  }
-
-  reflect(normal) {
-    const reflected = this.edges.map((e) => {
-      const dotP = e.dot(normal)
-      const multP = normal.mult(dotP * 2);
-      const res = e.copy().sub(multP)
-      return res;
-    });
-  }
-
-  draw() {
-    const p5 = this.p5;
-    p5.push();
-    p5.stroke(0, 255);
-    p5.fill(150, p5.random(150, 200), p5.random(150, 200), 100);
-    p5.translate(this.center.x, this.center.y);
-    p5.rotate(this.rotation);
-
-    p5.beginShape();
-    this.edges.forEach((e) => {
-      p5.vertex(e.x, e.y);
-    });
-    p5.endShape();
-    p5.pop();
+    this.translation = translation;
+    this.point = point;
   }
 }
+
+Hexagon.generatePoints = (start, length) => {
+  const start = p.createVector(start.x, start.y + length);
+  const points = [];
+  for(let i = 0; i < Hexagon.SIDES; i++) {
+    const newPoint = start.copy().rotate(Hexagon.ANGLE * i);
+    points.push(newPoint);
+  }
+  return points;
+}
+
+Hexagon.SIDES = 6;
+Hexagon.ANGLE = 360 / Hexagon.SIDES;
+
+export default Hexagon;
