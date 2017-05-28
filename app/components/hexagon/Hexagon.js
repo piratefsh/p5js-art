9717a056510bd150406e34ef42c4a7ed257799bc
Sher Minn Chong <sherminn.chong@gmail.com>
2017-05-28 10:32:46 +0800

    Refactor out recursive pattern

--- app/components/hexagon/Hexagon.js
+++ app/components/hexagon/Hexagon.js
@@ -1,7 +1,7 @@
 import { p } from 'P5Instance';
 
 export default class Hexagon {
-  constructor(centerPos, edgeLen, minLen= 10) {
+  constructor(centerPos, edgeLen, minLen = 10) {
     this.minLen = minLen < 0 ? 5 : minLen;
     this.id = Hexagon.ID++;
     this.vertices = [];
@@ -19,44 +19,46 @@ export default class Hexagon {
   }
 
   draw() {
-    if(this.edgeLen <= this.minLen){
-    p.push();
-    p.translate(this.centerPos.x, this.centerPos.y);
+    if (this.edgeLen <= this.minLen) {
+      p.push();
+      p.translate(this.centerPos.x, this.centerPos.y);
     // p.text(this.id, 0, 0)
-    p.beginShape();
-    p.stroke(255, 255, 255, 0)
-    p.fill(255, 255, 255);
-    this.vertices.forEach((v) => {
-      p.vertex(v.x, v.y);
-    });
-    p.endShape(p.CLOSE);
-    p.pop();
+      p.beginShape();
+      p.stroke(255, 255, 255, 0);
+      p.fill(255, 255, 255);
+      this.vertices.forEach((v) => {
+        p.vertex(v.x, v.y);
+      });
+      p.endShape(p.CLOSE);
+      p.pop();
       // draw a hex
-      return
+      return;
     }
 
     // draw recursive hexes
     const children = [];
-    const childLen = this.edgeLen/3;
+    const childLen = this.edgeLen / 3;
 
     const center = new Hexagon(this.centerPos, childLen, this.minLen);
     children.push(center);
 
-    //draw surrounding hexagons
-    const curr = p.createVector(0, childLen * 2);
-    const angle = Math.PI * 2 / 6;
-    for(let i = 0; i < 6; i++){
-      const currCenter = curr.copy().rotate(angle * i).add(this.centerPos)
-      // p.ellipse(currCenter.x, currCenter.y, 5, 5)
+    // draw surrounding hexagons
+    for (let i = 0; i < 6; i++) {
+      const currCenter = Hexagon.t3636(childLen, i, this.centerPos);
       const hex = new Hexagon(currCenter, childLen, this.minLen);
       children.push(hex);
     }
 
     children.forEach((ch) => ch.draw());
-
-    return ;
-
+    return;
   }
 }
 
+Hexagon.t3636 = (len, i, centerPos) => {
+  return p.createVector(0, len * 2)
+    .rotate(Hexagon.ANGLE * i)
+    .add(centerPos);
+};
+
+Hexagon.ANGLE = Math.PI * 2 / 6;
 Hexagon.ID = 0;
