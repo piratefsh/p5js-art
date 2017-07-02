b25f67749645bd61da5d4230f9e222d7af9cd846
Sher Minn Chong <sherminn.chong@gmail.com>
2017-07-02 13:36:17 +0800

    Get skew working

--- app/components/Radial.js
+++ app/components/Radial.js
@@ -1,27 +1,36 @@
-import { p as p5} from 'P5Instance';
+import { p as p5 } from 'P5Instance';
 
 export default class Radial {
-  constructor({ pos, radius = 100, len = 10, spokes = 50, color=0, strokeWeight=2 } = {}) {
+  constructor({ pos, radius = 100, len = 10, spokes = 50, color = 0, strokeWeight = 2, skewAngle = 0 } = {}) {
     this.pos = pos || p5.createVector(p5.width / 2, p5.height / 2);
     this.radius = radius;
     this.len = len;
     this.spokes = spokes;
     this.color = color;
-    this.strokeWeight = strokeWeight
+    this.strokeWeight = strokeWeight;
+    this.skewAngle = -skewAngle;
   }
 
   draw() {
     p5.push();
     p5.translate(this.pos.x, this.pos.y);
+    p5.rotate(this.skewAngle)
     p5.stroke(this.color);
-    p5.strokeWeight(this.strokeWeight)
-    const angleStep = p5.TWO_PI/this.spokes;
-    for(let i = 0; i < this.spokes; i++){
-      const x1 = this.radius * p5.cos(angleStep * i)
-      const y1 = this.radius * p5.sin(angleStep * i)
+    p5.strokeWeight(this.strokeWeight);
+    const angleStep = p5.TWO_PI / this.spokes;
+    const outerRad = this.radius + this.len;
+    const offset = p5.createVector(this.len * p5.cos(Math.PI * 1.5),
+      this.len * p5.sin(Math.PI * 1.5));
 
-      const x2 = (this.radius + this.len) * p5.cos(angleStep * i)
-      const y2 = (this.radius + this.len) * p5.sin(angleStep * i)
+    p5.line(0, 0, offset.x, offset.y)
+
+    for (let i = 0; i < this.spokes; i++) {
+      const angle = angleStep * i;
+      const x1 = this.radius * p5.cos(angle);
+      const y1 = this.radius * p5.sin(angle);
+      // (p5.map(angle - Math.PI, 0, Math.PI, 0, this.len));
+      const x2 = (outerRad * p5.cos(angle)) + offset.x;
+      const y2 = (outerRad * p5.sin(angle)) + offset.y;
 
       p5.line(x1, y1, x2, y2);
     }
--- app/components/RadialDrawer.js
+++ app/components/RadialDrawer.js
@@ -1,41 +1,46 @@
 import Radial from './Radial';
-import { p as p5} from 'P5Instance';
+import { p as p5 } from 'P5Instance';
 
-export default class RadialDrawer{
-  constructor(options){
+export default class RadialDrawer {
+  constructor(options) {
     this.options = options;
     this.speed = 5;
-    this.prevPos = p5.createVector(p5.width/2, p5.height/2);
+    this.prevPos = p5.createVector(p5.width / 2, p5.height / 2);
+    this.currPos = p5.createVector(p5.width / 2, p5.height / 2);
+    this.currVector = p5.createVector(0, 0);
     this.maxSpeed = 20;
   }
 
-  update(){
-    this.currPos = p5.createVector(p5.mouseX, p5.mouseY);
-    this.speed = Math.abs((this.currPos
-      .copy()
-      .sub(this.prevPos)
-      .mag()));
-    debugger;
+  update() {
+    this.currPos = p5.createVector(p5.mouseX - (p5.width / 2), p5.mouseY - (p5.height / 2));
+    const vector = this.currPos.copy().sub(this.prevPos);
+    this.speed = p5.abs(vector.mag());
+
+    // update currvector if its not empty
+    if (vector.x != 0 && vector.y != 0) {
+      this.currVector = vector;
+    }
     this.prevPos = this.currPos;
   }
 
-  draw(){
+  draw() {
     const options = {
+      skewAngle: p5.atan2(this.currVector.x, this.currVector.y),
       pos: p5.createVector(p5.mouseX, p5.mouseY),
       len: p5.map(this.speed, 0, this.maxSpeed, RadialDrawer.MIN_LEN, RadialDrawer.MAX_LEN),
       spokes: p5.map(this.speed, 0, this.maxSpeed, RadialDrawer.MIN_SPOKES, RadialDrawer.MAX_SPOKES),
       radius: p5.map(this.speed, 0, this.maxSpeed, RadialDrawer.MAX_RADIUS, RadialDrawer.MIN_RADIUS),
-    }
+    };
     const r = new Radial(Object.assign(options, this.options));
     r.draw();
   }
 }
 
-RadialDrawer.MIN_RADIUS = 5;
-RadialDrawer.MAX_RADIUS = 30;
+RadialDrawer.MIN_RADIUS = 20;
+RadialDrawer.MAX_RADIUS = 20;
 
-RadialDrawer.MIN_LEN = 5;
-RadialDrawer.MAX_LEN = 20;
+RadialDrawer.MIN_LEN = 10;
+RadialDrawer.MAX_LEN = 10;
 
-RadialDrawer.MIN_SPOKES = 10;
-RadialDrawer.MAX_SPOKES = 70;
+RadialDrawer.MIN_SPOKES = 50;
+RadialDrawer.MAX_SPOKES = 50;
