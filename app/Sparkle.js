8e50ef0a2151bbf36dd33f8a953c0ee87aec63ea
Sher Minn Chong <sherminn.chong@gmail.com>
2017-11-27 16:25:26 +0100

    Finish

--- app/Sparkle.js
+++ app/Sparkle.js
@@ -6,12 +6,12 @@ export default class Sparkle {
     this.rings = [];
     this.numRings = 5;
     for (let i = 0; i < this.numRings; i++) {
-      this.rings.push(new Ring({ minRadius: 100, maxRadius: p.width / 4 }));
+      this.rings.push(new Ring({ minRadius: 150, maxRadius: p.width / 2 }));
     }
     this.step = p.TWO_PI / this.numRings;
     this.radius = 100;
     this.minRadius = 100;
-    this.maxRadius = p.width / 4;
+    this.maxRadius = 200;
   }
 
   draw() {
@@ -19,19 +19,30 @@ export default class Sparkle {
     p.translate(p.width / 2, p.height / 2);
     p.rotate(this.angle);
     p.translate(-this.radius / 2, -this.radius / 2);
+    p.fill(255, 190);
+    this.drawRings();
+
+    p.push();
+    p.fill(255, 10);
+    p.scale(1.5);
+    this.drawRings();
+    p.pop();
+    p.pop();
+  }
+
+  drawRings() {
     this.rings.forEach((r, i) => {
       const tx = this.radius * Math.cos(i * this.step);
       const ty = this.radius * Math.sin(i * this.step);
       p.translate(tx, ty);
       r.draw();
     });
-    p.pop();
   }
 
   update() {
     this.angle = p.map(pointer.x, 0, p.width, 0, p.PI / 2);
     pointer.update();
-    this.radius = p.map(pointer.x, 0, p.width, this.minRadius, this.maxRadius);
+    this.radius = p.map(pointer.x, p.width, 0, this.minRadius, this.maxRadius);
     this.rings.forEach((r) => {
       r.update();
     });
@@ -40,11 +51,12 @@ export default class Sparkle {
 
 class Ring {
   constructor(props) {
+    this.minRadius = props.minRadius;
+    this.maxRadius = props.maxRadius;
     this.radius = props.minRadius;
     this.glowRadius = 5 + Math.floor(p.random(7));
     this.size = 2 + Math.floor(p.random(15));
 
-    console.log(this.size, this.glowRadius)
     this.numSteps = 24;
     this.points = [];
     const step = p.TWO_PI / this.numSteps;
@@ -57,8 +69,6 @@ class Ring {
 
   draw() {
     p.noStroke();
-    p.fill(255, 190);
-
     this.points.forEach((point) => {
       p.push();
       p.translate(point.x, point.y);
@@ -69,7 +79,7 @@ class Ring {
   }
 
   update() {
-    this.radius = p.map(pointer.y, 0, p.height, 150, p.height);
+    this.radius = p.map(pointer.y, 0, p.height, this.minRadius, this.maxRadius);
 
     this.points.forEach((pt) => {
       pt.x = this.radius * Math.cos(pt.angle);
