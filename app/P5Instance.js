74b0ae137ab8563da308a8cbe1d564ccefba3826
Sher Minn Chong <sherminn.chong@gmail.com>
2017-11-28 13:10:58 +0100

    Parameterize location and size

--- app/P5Instance.js
+++ app/P5Instance.js
@@ -1,18 +1,33 @@
 import p5 from 'p5';
 import Util from 'components/utils/Utils';
 import ParametricPatterns from 'components/ParametricPatterns';
+import dat from 'dat.gui/build/dat.gui';
+
+const gui = new dat.GUI();
+
 const sketch = p => {
-  const gridX = 1;
-  const gridY = 1;
+  const gridX = 2;
+  const gridY = 2;
   const canvasSize = 600;
   const cellSize = Math.ceil(canvasSize / gridX);
   const edgeLen = cellSize / 2;
-  const pp = new ParametricPatterns();
+  const patterns = [];
   p.setup = () => {
     p.createCanvas(canvasSize, canvasSize / gridX * gridY);
     p.reset();
     // p.noLoop();
     p.frameRate(60);
+    for(let i = 0; i < gridX; i++){
+      for(let j = 0; j < gridY; j++){
+        const pp = new ParametricPatterns({
+          x: i * cellSize,
+          y: j * cellSize,
+          width: cellSize,
+          height: cellSize,
+        });
+        patterns.push(pp);
+      }
+    }
   };
 
   p.reset = () => {
@@ -20,8 +35,11 @@ const sketch = p => {
 
   p.draw = () => {
     p.background(0);
-    pp.update();
-    pp.draw();
+
+    patterns.forEach((pp) => {
+      pp.update();
+      pp.draw();
+    })
   };
 
   p.keyPressed = () => {
--- app/components/ParametricPatterns.js
+++ app/components/ParametricPatterns.js
@@ -2,7 +2,11 @@ import { p } from 'P5Instance';
 
 export default class ParametricPatterns {
 
-  constructor() {
+  constructor(props) {
+    this.props = props;
+    this.padding = 20;
+    this.width = (props.width || p.width) - this.padding;
+    this.height = (props.height || p.height) - this.padding;
     this.step = 0.03;
     this.t = 0;
     this.numLines = 60;
@@ -11,12 +15,13 @@ export default class ParametricPatterns {
   draw() {
     const debug = false;
     p.push();
+    p.translate(this.props.x + this.padding/2, this.props.y + this.padding/2)
     p.strokeWeight(0.9);
-    const count = this.numLines * 0.1
-    // p.fill(255, p.map(p.mouseY, 0, p.height, 0, 8));
+    const count = this.numLines * 0.1;
+    // p.fill(255, p.map(p.mouseY, 0, this.height, 0, 8));
     p.fill(255, 10);
-    p.translate(p.width / 2, p.height / 2);
-    p.curveTightness(p.map(p.mouseX, 0, p.width, -4, 4))
+    p.translate(this.width / 2, this.height / 2);
+    p.curveTightness(2);
     for (let i = 0; i < count; i += 0.1) {
       const t = this.t + i;
       p.stroke(255, p.map(i, 0, count, 0, 200));
@@ -38,35 +43,35 @@ export default class ParametricPatterns {
   }
 
   x1(t) {
-    return 200 * p.sin(t / 2);
+    return this.width/2 * p.sin(t / 2);
   }
 
   y1(t) {
-    return 200 * p.cos(t / 2);
+    return this.height/2 * p.cos(t / 2);
   }
 
   x2(t) {
-    return 100 * p.cos(t / 4);
+    return this.width/3 * p.cos(t / 4);
   }
 
   y2(t) {
-    return 150 * p.sin(t / 2);
+    return this.height/2 * p.sin(t / 2);
   }
 
   cx1(t) {
-    return 100 * p.cos(t);
+    return this.width/2 * p.cos(t);
   }
 
   cy1(t) {
-    return 100 * p.sin(t);
+    return this.height/2 * p.sin(t);
   }
 
   cx2(t) {
-    return -100 * p.sin(t / 2);
+    return -this.width/2 * p.sin(t / 2);
   }
 
   cy2(t) {
-    return -100 * p.cos(t);
+    return -this.height/2 * p.cos(t);
   }
 }
 
