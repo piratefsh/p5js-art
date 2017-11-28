5a9857c1b68db10a503385788b11b5e52b47cdef
Sher Minn Chong <sherminn.chong@gmail.com>
2017-11-28 12:28:46 +0100

    Use curve instead

--- app/components/ParametricPatterns.js
+++ app/components/ParametricPatterns.js
@@ -5,50 +5,64 @@ export default class ParametricPatterns {
   constructor() {
     this.step = 0.02;
     this.t = 0;
-    this.numLines = 5;
+    this.numLines = 10;
   }
 
   draw() {
-    if(this.t > p.TWO_PI * 2){
-      // return
-    }
+    const debug = false;
     p.push();
-    p.strokeWeight(3);
-    p.translate(p.width/2, p.height/2)
-    for(let i = 0; i < this.numLines; i+=0.1){
+    p.strokeWeight(0.9);
+    p.fill(255, 20);
+    p.translate(p.width / 2, p.height / 2);
+    for (let i = 0; i < this.numLines; i += 0.1) {
       const t = this.t + i;
-      // p.fill(255, 0, 0)
-      // p.ellipse(this.x1(t), this.y1(t), 10, 10);
-      // p.fill(0, 255, 0)
-      // p.ellipse(this.x2(t), this.y2(t), 10, 10);
-
-      
       p.stroke(255, p.map(i, 0, this.numLines, 0, 200));
-      p.line(this.x1(t), this.y1(t), this.x2(t), this.y2(t));
-      // console.log(this.x1(i), this.y1(i), this.x2(i), this.y2(i));
+      p.curve(this.cx1(t), this.cy1(t), this.x1(t), this.y1(t), this.x2(t), this.y2(t), this.cx2(t), this.cy2(t));
+
+      if (debug) {
+        p.ellipse(this.x1(t), this.y1(t), 10, 10);
+        p.ellipse(this.cx1(t), this.cy1(t), 5, 5);
+        p.ellipse(this.x2(t), this.y2(t), 10, 10);
+        p.ellipse(this.cx2(t), this.cy2(t), 5, 5);
+      }
     }
     p.pop();
   }
 
   update() {
-    this.t = (this.t + this.step) 
-
+    this.t = (this.t + this.step);
   }
 
   x1(t) {
-    return 200 * p.sin(t/5);
+    return 200 * p.sin(t / 2);
   }
 
   y1(t) {
-    return p.sin(t/5) + p.cos(t/5) * 200;
+    return 200 * p.cos(t / 2);
   }
 
   x2(t) {
-    return 100 * p.cos(t);
+    return 100 * p.cos(t / 4);
   }
 
   y2(t) {
-    return 200 * p.sin(t/4) + 100 * p.sin(t/2);
+    return 150 * p.sin(t / 2);
+  }
+
+  cx1(t) {
+    return 100 * p.cos(t);
+  }
+
+  cy1(t) {
+    return 100 * p.sin(t);
+  }
+
+  cx2(t) {
+    return -100 * p.sin(t / 2);
+  }
+
+  cy2(t) {
+    return -100 * p.cos(t);
   }
 }
 
