611d7ad7356b28c92ac676e2a3881506777a6844
Sher Minn Chong <sherminn.chong@gmail.com>
2017-11-29 17:25:15 +0100

    Create sneznt unit

--- app/P5Instance.js
+++ app/P5Instance.js
@@ -1,23 +1,30 @@
 import p5 from 'p5';
 import Util from 'components/utils/Utils';
+import SnezntUnit from 'components/SnezntUnit';
 const sketch = p => {
   const gridX = 1;
   const gridY = 1;
   const canvasSize = 600;
   const cellSize = Math.ceil(canvasSize / gridX);
   const edgeLen = cellSize / 2;
+  let sz;
+
   p.setup = () => {
     p.createCanvas(canvasSize, canvasSize / gridX * gridY);
     p.reset();
     p.noLoop();
     p.frameRate(60);
+    p.reset();
   };
 
   p.reset = () => {
+    sz = new SnezntUnit();
   };
 
   p.draw = () => {
     p.background(200);
+    sz.update();
+    sz.draw();
   };
 
   p.keyPressed = () => {
---
+++ app/components/SnezntUnit.js
@@ -0,0 +1,37 @@
+import { p } from 'P5Instance';
+
+export default class SnezntUnit {
+  constructor() {
+    this.numRings = 5;
+    this.radius = 100;
+    this.center = { x: p.width/2, y: p.height/2 };
+
+    this.points = [];
+
+    const vertex = p.createVector(0, 0);
+
+    for (let i = 0; i < 4; i++) {
+      const vx = vertex.copy()
+        .add(this.radius, 0)
+        .rotate(p.TWO_PI / 4 * i + p.PI/4);
+      this.points.push(vx);
+    }
+  }
+
+  draw() {
+    p.push();
+    p.fill(0, 0);
+    p.translate(this.center.x, this.center.y);
+    p.beginShape();
+    this.points.forEach((pt) => {
+      p.vertex(pt.x, pt.y);
+    });
+    p.endShape(p.CLOSE);
+    p.pop();
+  }
+
+  update() {
+    this.center.x += 1;
+    this.center.y += 1;
+  }
+}
