3b27af30db43546002dd7b8de6cb2645fb78d84d
Sher Minn Chong <sherminn.chong@gmail.com>
2017-12-05 13:39:04 +0100

    working

--- app/P5Instance.js
+++ app/P5Instance.js
@@ -1,30 +1,40 @@
 import p5 from 'p5';
-import thing from 'p5/lib/addons/p5.dom';
 import Util from 'components/utils/Utils';
 import dat from 'dat.gui-0.6.5/build/dat.gui';
 import EyeSeeYou from 'EyeSeeYou';
-console.log('thing', thing)
-console.log('thing', p5.createVideo)
-// Object.assign(p5, p5dom);
-const sketch = p => {
-  const gridX = 1;
-  const gridY = 1;
-  const canvasSize = 600;
-  const cellSize = Math.ceil(canvasSize / gridX);
-  const edgeLen = cellSize / 2;
-  let gui = new dat.GUI();
-  let esy = new EyeSeeYou();
 
+const canvasHolder = document.createElement('div');
+canvasHolder.id = 'canvas-holder';
+document.body.appendChild(canvasHolder);
+
+const sketch = p => {
+  const canvasSize = 480;
+  const canvasHeight = 360;
+  const gui = new dat.GUI();
+  let esy;
   p.setup = () => {
-    p.createCanvas(canvasSize, canvasSize / gridX * gridY);
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas-holder').children[0];
+    video.style.width = `${canvasSize}px`;
+    video.style.height = `${canvasHeight}px`;
+    // hide video
+    video.style.position = 'fixed';
+    video.style.bottom = '99999px';
+
+    p.createCanvas(canvasSize, canvasHeight);
     p.reset();
     p.noLoop();
     p.frameRate(60);
+
+    esy = new EyeSeeYou({
+      video,
+      canvas,
+    });
     esy.setup();
   };
 
   p.reset = () => {
-    p.createVector(10, 0).copy()
+    p.createVector(10, 0).copy();
   };
 
   p.draw = () => {
@@ -47,5 +57,5 @@ const sketch = p => {
 };
 
 // set global functions for p5
-const p = new p5(sketch);
+const p = new p5(sketch, canvasHolder);
 export { p };
